ecd23fe866325f317318ff6f07d38accd2401036
sirikwan <sirikuan1@hotmail.com>
2018-04-27 15:05:48 +0700

    Add car analytics Feature

---
+++ CarAnalytics.py
@@ -0,0 +1,12 @@
+class LicencePlate:
+   def __init__(self):
+       pass
+
+
+   def process(self, filename):
+       pass    
+
+__name__=='__main__':
+lp = LicencePlate()
+filename = 'car01.jpg'        
+result = lp.process(filename)
--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, abort
 
+import tempfile
+import os
+import sys
+
+from Feature.CarAnalytics import LicencePlate
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -8,17 +14,39 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    ImageMessage, VideoMessage, AudioMessage
 )
+
 import oil_price
 
 app = Flask(__name__)
 
+latest_image_path = ""
+
 line_bot_api = LineBotApi('zJpp5I1+RnkZ0hipU9suQMvxBEPis5aZyICsBI6aetecVdBOvXwDT17hsqaRjSxxM1+MBCzQVmyTZ6Sbzonq8plbtlg58gt1ebG34kSsXy8R0owIidmZPDs2BLNrRUSTH8eHZsZHs6+8JEVb5uTrqAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('3b48e125e82f410a2a4a4945eeaffe41')
 
-@app.route("/",methods=['GET'])
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+# function for create tmp dir for download content
+def make_static_tmp_dir():
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+
+
+@app.route("/", methods=['GET'])
 def default_action():
-    return 'Hello World'
+    l = oil_price.get_prices()
+    s = ""
+    for p in l:
+        s += "%s %f บาท\n"%(p[0],p[1])
+    return s
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,6 +68,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    global latest_image_path
+
     if event.message.text == 'ราคาน้ำมัน':
         l = oil_price.get_prices()
         s = ""
@@ -49,10 +79,64 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=s))
+    elif event.message.text == 'วิเคราะห์รูป':
+        line_bot_api.reply_message(
+            event.reply_token, [
+                TextSendMessage(text='สักครู่ค่ะ')
+            ])
+
+    # Process image
+    try:
+        lp = LicencePlate()
+        result = lp.process(latest_image_path)
+        s = lp.translate(result)
+
+        line_bot_api.push_message(
+            event.source.user_id, [
+                TextSendMessage(text=s)
+            ])
+    except Exception as e:
+        print('Exception:',type(e),e)
+        line_bot_api.push_message(
+            event.source.user_id, [
+                TextSendMessage(text='ไม่สามารถวิเคราะห์รูปได้')
+            ])
     else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text+'จ้า'))
+
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    global latest_image_path
+
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    else:
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    # Save image path
+    latest_image_path = dist_path
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='เก็บรูปให้แล้วค่ะ')
+        ])
+
+
 
 if __name__ == "__main__":
     app.run()
